b848afe76efb2f7633a8e485ddff114ee7dce9d1
YTMcGamer <ytmcgamer99@gmail.com>
2020-09-26 17:06:09 +0200

    spoofer: anonfiles.com/JdgasdZco3/HWID_Spoofer_exe

---
+++ start all cleaners.bat
@@ -0,0 +1,16 @@
+@echo off 
+start 1.bat
+start 2.bat
+start 3.bat
+start 4.bat
+start 5.bat
+start 6.bat
+start 7.bat
+start 8.bat
+start 9.bat
+start 10.bat
+start 11.bat
+start 12.bat
+start 13.bat
+start 14.bat
+exit
